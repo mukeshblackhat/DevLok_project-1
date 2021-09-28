c6f1e0f5b779bd3f293ec1cd7df05be67f5ba4ce
robin00007 <76676218+robin00007@users.noreply.github.com>
2021-09-28 16:32:47 +0530

    project done

--- devlok/src/components/Resources.js
+++ devlok/src/components/Resources.js
@@ -34,49 +34,54 @@ const Resources = () => {
         });
       });
   });
-
-  if (loading.load == true) {
+  const jwt = JSON.parse(localStorage.getItem("key"));
+  if (jwt != "login ho gaya") {
     return (
       <div className="editor">
-        <Loading location="Finding resources" />
+        <Wait />
       </div>
     );
-  }
-   
-  else {
-    return (
-      <div className="editor add2">
-        <h3>My name is Resources</h3>
-        <br />
-
-        <div className="Lobby_container">
-          <p>
-            this is the area where you can find all of your notes . So don't
-            worry just keep learning we keep your data safe for you
-          </p>
-          {resources.map((note) => {
-            return (
-              <div className="Note_output_box">
-                <h3>{note.topic}</h3>
-                <p>
-                  <i>
-                    {note.subject}{" "}
-                    <button className="more_button" onClick={More_action}>
-                      .....more
-                    </button>
-                  </i>
-                </p>
-
-                <p className="author_name_lobby">
-                  <stong>BY :{note.author}</stong>
-                </p>
-              </div>
-            );
-          })}
+  } else {
+    if (loading.load == true) {
+      return (
+        <div className="editor">
+          <Loading location="Finding resources" />
         </div>
-        
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="editor add2">
+          <h3>My name is Resources</h3>
+          <br />
+
+          <div className="Lobby_container">
+            <p>
+              this is the area where you can find all of your notes . So don't
+              worry just keep learning we keep your data safe for you
+            </p>
+            {resources.map((note) => {
+              return (
+                <div className="Note_output_box">
+                  <h3>{note.topic}</h3>
+                  <p>
+                    <i>
+                      {note.subject}{" "}
+                      <button className="more_button" onClick={More_action}>
+                        .....more
+                      </button>
+                    </i>
+                  </p>
+
+                  <p className="author_name_lobby">
+                    <stong>BY :{note.author}</stong>
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
   }
 };
 
